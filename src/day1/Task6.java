c8506908b3d781d2e0298a1bc5adad6d46b0cd7b
Mannanov Ildar <iimannanov@mail.ru>
2021-09-04 22:47:49 +0400

    Day 1

---
+++ src/day1/Task6.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int nomer = 5;
+        for (int j = 1; j <= 10; j++) {
+                System.out.println(j + " х " + nomer + " = " + j * nomer);
+        }
+    }
+}
